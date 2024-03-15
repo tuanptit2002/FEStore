402be5ea0c229f963ffedb12a4086c7e9c190a68
tientato2002 <tientato2002@gmail.com>
2024-03-15 15:58:17 +0700

    test
    Signed-off-by: tientato2002 <tientato2002@gmail.com>

---
+++ src/Componets/Footer/Footer.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <div class="footer-container">
+                    <div class="footer-contact">
+                        <h2>Hệ thống Cửa hàng</h2>
+                        <p>Địa chỉ:</p>
+                        <ul>
+                            <li>27 Chúa Bộc, Đống Đa, HN</li>
+                            <li>242 Thái Hà, Đống Đa, HN</li>
+                            <li>63 Đại Cổ Việt, Hai Bà Trưng, HN</li>
+                            <li>296 Nguyễn Trãi, Hà Đông, HN</li>
+                            <li>24. Trần Phú, Hà Đông, HN</li>
+                            <li>69 Quang Trung, Hà Đông, HN</li>
+                        </ul>
+                    </div>
+
+                    <div class="footer-police">
+                        <h2>CHÍNH SÁCH VÀ QUY ĐỊNH CHUNG</h2>
+                        <ul>
+                            <li><a href="#">Hướng Dẫn Mua Hàng</a></li>
+                            <li><a href="#">Hình Thức Thanh Toán</a></li>
+                            <li><a href="#">Quy Định Về Bảo Mật Thông Tin</a></li>
+                            <li><a href="#">Chính Sách Bảo Hành</a></li>
+                            <li><a href="#">Chính Sách Đổi Hàng</a></li>
+                            <li><a href="#">Chính Sách Vận Chuyển</a></li>
+                            <li><a href="#">Điều Khoản Dịch Vụ</a></li>
+                            <li><a href="#">Giới Thiệu</a></li>
+
+
+                        </ul>
+                    </div>
+                    <div class="footer-adress">
+                        <h2>ĐỊA CHỈ</h2>
+                        <ul>
+                            <li><a href="#">CÔNG TY CỔ PHẦN THỜI TRANG 360</a></li>
+                            <li><a href="#">VPGD: Đội 6, Xã Phương Đình, Huyện Đan Phượng,
+                                Thành phố Hà nội, Việt nam</a></li>
+                            <li><a href="#">Facebook thời trang nam:
+                                360Boutique</a></li>
+                            <li><a href="#">Hotline: 0973285886 – 0862052988
+                                Email:</a></li>
+                            <li><a href="#">360boutique.vn@gmail.com</a></li>
+                            <li><a href="#">
+                                Website:
+                                https://360boutique.vn/</a></li>
+
+
+
+                        </ul>
+                    </div>
+                    <div class="footer-icon">
+                        <h2>ĐỊA CHỈ</h2>
+                        <ul>
+                            <li>
+
+                            </li>
+
+
+
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="footer-copyright">
+                    <p>&copy; 2024 Hệ thống Cửa hàng</p>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
