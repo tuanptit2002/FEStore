afabc2086f0d80509d0cdfbef584e2febf5ff50d
tientato2002 <tientato2002@gmail.com>
2024-03-20 21:08:38 +0700

    footerNew

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Componets/Navbar/Navbar";
+import Footer from './Componets/footer/Footer';
 
 function App() {
   return (
     <div >
-     <Navbar/>
+      <Navbar />
+      <Footer></Footer>
     </div>
   );
 }
